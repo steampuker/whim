889184cadde499779952cfedd3caa2342f2e5286
steampuker <214028729+steampuker@users.noreply.github.com>
2026-02-26 19:40:38 +0500

    Replaced the ugly WHIM_SEND_REQUEST macro with proper inline functions, with assertions included Reduced the timeout for polling from infinite to 5 seconds.

--- whim.h
+++ whim.h
@@ -1,5 +1,4 @@
 /* See the end of file for license information. */
-
 #ifndef WHIM_H
 #define WHIM_H
 #if !defined(WHIM_NO_STDINT)
@@ -76,29 +75,30 @@ typedef struct WhimHook WhimHook;
 
 enum {
     WHIM_EVENT_NONE,
+    WHIM_EVENT_INTERNAL,
     WHIM_EVENT_KEY,
     WHIM_EVENT_CLOSE
 };
 
-struct WhimKeyEvent {
+typedef struct WhimEventKey {
     whim_u32 type;
     whim_u32 keysym;
     whim_u8 keycode;
     whim_bool is_pressed;
-};
+} WhimEventKey;
 
-struct WhimCloseEvent {
+typedef struct WhimEventClose {
     whim_u32 type;
     whim_u32 window_id;
-};
+} WhimEventClose;
 
 // struct WhimMouseMotionEvent {};
 // struct WhimMouseButtonEvent {};
 
 typedef union WhimEvent {
     whim_u32 type;
-    struct WhimKeyEvent as_key;
-    struct WhimCloseEvent as_close;
+    WhimEventKey as_key;
+    WhimEventClose as_close;
     // struct WhimMouseMotionEvent as_mouse_motion;
     // struct WhimMouseButtonEvent as_mouse_button;
 } WhimEvent;
@@ -208,13 +208,11 @@ whim_bool whimWinShouldClose(WhimWin *window);
 #define WHIM_SANITIZERS_ON
 #endif
 
-struct whim_xcb__iovec { void* base; int len; };
+struct whim_xcb__iovec { void* base; size_t len; };
 struct whim_xcb__req_t { size_t count; void *ext; whim_u8 opcode, isvoid; };
 
 typedef union { whim_u8 as_8[4]; whim_u16 as_16[2]; whim_u32 as_32; } WhimPayload;
 #define WHIM_ARRLEN(x) (sizeof(x) / sizeof(*x))
-
-#define WHIM_SEND_REQUEST(hook, payload, payload_size) x11.sendRequest(hook.connection, 2, (struct whim_xcb__iovec[2]){{payload, payload_size}}, (struct whim_xcb__req_t[1]){1, 0, 0, 1});
 
 struct WhimWin {
     whim_u32 window_id;
@@ -237,7 +235,7 @@ static struct WhimX11State {
     void (*disconnect)(void*);
     int (*getFileDesc)(void*);
     whim_u32 (*generateID)(void*);
-    int (*sendRequest)(void*, int, struct whim_xcb__iovec*, const struct whim_xcb__req_t*);
+    int (*sendRequest)(void*, int, struct whim_xcb__iovec*, struct whim_xcb__req_t*);
     void* (*checkEventQueue)(void*);
     void* (*getReply)(void*, unsigned int, void**);
     void (*free)(void*);
@@ -252,9 +250,35 @@ static struct WhimX11State {
     whim_u32 screens, root_window;
 } x11;
 
+
+WHIM_UTIL void whimXcbSendRequest(const WhimHook* WHIM_NOALIAS hook, void* WHIM_NOALIAS payload, size_t payload_length) {
+    struct whim_xcb__req_t req[1] = {1, 0, 0, 1};
+    struct whim_xcb__iovec iovec[2] = {{payload, payload_length}};
+    int result = x11.sendRequest(hook->connection, 2, iovec, req);
+    WHIM_ASSERT(result, "Invalid data handling");
+}
+
+WHIM_UTIL void whimXcbSendMultiRequest(const WhimHook* WHIM_NOALIAS hook, const whim_u32 count, void* WHIM_NOALIAS buffers[], const whim_u32 lengths[])
+{
+    enum { IOVEC_MAX = 4 };
+    WHIM_ASSERT(count * 2 <= IOVEC_MAX, "iovec limit exceeded");
+
+    struct whim_xcb__req_t req[1] = {count * 2, 0, 0, 1};
+    struct whim_xcb__iovec iovec[IOVEC_MAX];
+    for(int i = 0; i < count; ++i) {
+        iovec[i * 2].base = buffers[i];
+        iovec[i * 2].len = lengths[i];
+        iovec[i * 2 + 1].base = buffers[i];
+        iovec[i * 2 + 1].len = -lengths[i] & 3;
+    }
+
+    int result = x11.sendRequest(hook->connection, 2, iovec, req);
+    WHIM_ASSERT(result, "Invalid data handling");
+}
+
 WHIM_UTIL whim_bool whimPollReceive(whim_u32 file_desc, WhimPayload *receiver, whim_u32 size)
 {
-    enum { POLL_TIMEOUT = -1 };
+    enum { POLL_TIMEOUT = 5000 };
     struct pollfd fd_poll[1] = {file_desc, POLLIN};
 
     if(poll(fd_poll, 1, POLL_TIMEOUT) <= 0)
@@ -266,24 +290,24 @@ WHIM_UTIL whim_bool whimPollReceive(whim_u32 file_desc, WhimPayload *receiver, w
     return 1;
 }
 
-#define X11_INTERN_ATOM(con, str) x11String8Req(con, 16, str, sizeof(str) - 1)
-#define X11_QUERY_EXT(con, str) x11String8Req(con, 98, str, sizeof(str) - 1)
-WHIM_UTIL void x11String8Req(void* WHIM_NOALIAS con, whim_u8 req, char* WHIM_NOALIAS str, whim_u32 str_len)
+#define X11_INTERN_ATOM(hook, str) x11String8Req(hook, 16, str, sizeof(str) - 1)
+#define X11_QUERY_EXT(hook, str) x11String8Req(hook, 98, str, sizeof(str) - 1)
+WHIM_UTIL void x11String8Req(const WhimHook* WHIM_NOALIAS hook, whim_u8 req, char* WHIM_NOALIAS str, whim_u32 str_len)
 {
     WhimPayload buffer[2] = {{req}}; buffer->as_16[1] = WHIM_ARRLEN(buffer) + (str_len + 3) / 4, buffer[1].as_16[0] = str_len;
-
-    struct whim_xcb__iovec vecs[4] = {{&buffer, sizeof(buffer)}, {0}, {str, str_len}, {&buffer, -str_len & 3}};
-    x11.sendRequest(con, 2, vecs, (struct whim_xcb__req_t[1]){4, 0, 0, 1});
+    void *ptr[] = {buffer, str};
+    whim_u32 lengths[] = {sizeof buffer, str_len};
+    whimXcbSendMultiRequest(hook, WHIM_ARRLEN(ptr), ptr, lengths);
 }
 
 WHIM_UTIL whim_u32* x11ScreenOfDisplay(void *con, int screen)
 {
-    struct xcb_iterator {whim_u32 *data; int rem; int index;} iter;
-    void* (*const getSetup)(void*) = dlsym(x11.lib, "xcb_get_setup");
-    void (*const screenNext)(struct xcb_iterator*) = dlsym(x11.lib, "xcb_screen_next");
-    struct xcb_iterator (*rootsIterator)(const void*) = dlsym(x11.lib, "xcb_setup_roots_iterator");
+    typedef struct {whim_u32 *data; int rem; int index;} xcb_iterator;
+    void* (*getSetup)(void*) = dlsym(x11.lib, "xcb_get_setup");
+    void (*screenNext)(xcb_iterator*) = dlsym(x11.lib, "xcb_screen_next");
+    xcb_iterator (*rootsIterator)(void*) = dlsym(x11.lib, "xcb_setup_roots_iterator");
 
-    iter = rootsIterator(getSetup(con));
+    xcb_iterator iter = rootsIterator(getSetup(con));
     for (; iter.rem; screen -= 1, screenNext(&iter))
         if(screen <= 0)
             return iter.data;
@@ -304,13 +328,13 @@ WHIM_UTIL void x11RoundtripAtoms(whim_u32 file_desc)
     }
 }
 
-WHIM_UTIL void x11RoundtripExtensions(WhimHook hook)
+WHIM_UTIL void x11RoundtripExtensions(WhimHook* hook)
 {
     enum {XKB_MAJOR = 1, XKB_MINOR = 0};
     WhimPayload xkb_receiver[8];
 
-    if(whimPollReceive(hook.file_desc, xkb_receiver, sizeof xkb_receiver)) do { // Initialize xkb
-        if(!xkb_receiver[0].as_8[0] || !xkb_receiver[2].as_8[0])
+    if(whimPollReceive(hook->file_desc, xkb_receiver, sizeof xkb_receiver)) do { // Initialize xkb
+        if(!xkb_receiver->as_8[0] || !xkb_receiver[2].as_8[0])
             break; // xkb is not present
 
         whim_u8 opcode = xkb_receiver[2].as_8[1];
@@ -321,19 +345,21 @@ WHIM_UTIL void x11RoundtripExtensions(WhimHook hook)
         buffer[1].as_16[0] = XKB_MAJOR, buffer[1].as_16[1] = XKB_MINOR;
         buffer2[1].as_16[0] = 256, buffer2[2].as_32 = 1, buffer2[3].as_32 = 1;
 
-        WHIM_SEND_REQUEST(hook, &buffer, sizeof(buffer));
-        WHIM_SEND_REQUEST(hook, &buffer2, sizeof(buffer2));
+        void* ptrs[] = {buffer, buffer2};
+        whim_u32 lengths[] = {sizeof buffer, sizeof buffer2};
+
+        whimXcbSendMultiRequest(hook, WHIM_ARRLEN(ptrs), ptrs, lengths);
 
         x11.flush(x11.hook.connection);
 
-        if(!whimPollReceive(hook.file_desc, xkb_receiver, sizeof xkb_receiver) ||
-           !xkb_receiver[0].as_8[0] || !xkb_receiver[2].as_8[0])
+        if(!whimPollReceive(hook->file_desc, xkb_receiver, sizeof xkb_receiver) ||
+           !xkb_receiver->as_8[0] || !xkb_receiver[2].as_8[0])
             break; // Can't use xkb
 
         x11.hook.xkb = opcode;
 
-        if(!whimPollReceive(hook.file_desc, xkb_receiver, sizeof xkb_receiver) ||
-           !xkb_receiver[0].as_8[0] || !xkb_receiver[2].as_8[0])
+        if(!whimPollReceive(hook->file_desc, xkb_receiver, sizeof xkb_receiver) ||
+           !xkb_receiver->as_8[0] || !xkb_receiver[2].as_8[0])
             break; // Autorepeat unsupported
     } while(0);
 }
@@ -357,12 +383,12 @@ WHIM_API(whim_bool whimInit, X11)(enum WhimInitFlags flags)
     x11.sendRequest = dlsym(x11.lib, "xcb_send_request");
     x11.flush = dlsym(x11.lib, "xcb_flush");
 
-    X11_INTERN_ATOM(x11.hook.connection, "WM_PROTOCOLS");
-    X11_INTERN_ATOM(x11.hook.connection, "WM_DELETE_WINDOW");
-    X11_INTERN_ATOM(x11.hook.connection, "_NET_WM_NAME");
-    X11_INTERN_ATOM(x11.hook.connection, "UTF8_STRING");
+    X11_INTERN_ATOM(&x11.hook, "WM_PROTOCOLS");
+    X11_INTERN_ATOM(&x11.hook, "WM_DELETE_WINDOW");
+    X11_INTERN_ATOM(&x11.hook, "_NET_WM_NAME");
+    X11_INTERN_ATOM(&x11.hook, "UTF8_STRING");
 
-    X11_QUERY_EXT(x11.hook.connection, "XKEYBOARD");
+    X11_QUERY_EXT(&x11.hook, "XKEYBOARD");
     //X11_QUERY_EXT(x11.hook.con, "RANDR");
 
     x11.flush(x11.hook.connection);
@@ -382,7 +408,7 @@ WHIM_API(whim_bool whimInit, X11)(enum WhimInitFlags flags)
     x11.root_window = *root_window_ptr;
 
     x11RoundtripAtoms(x11.hook.file_desc);
-    x11RoundtripExtensions(x11.hook);
+    x11RoundtripExtensions(&x11.hook);
 
     return WHIM_TRUE;
 
@@ -406,13 +432,13 @@ WHIM_UTIL void x11CreateWindow(WhimWin *win, whim_u32 parent)
     buffer[2].as_32 = parent;
     buffer[4].as_16[1] = buffer[4].as_16[0] = 1;
 
-    WHIM_SEND_REQUEST(win->hook[0], &buffer, sizeof(buffer));
+    whimXcbSendRequest(win->hook, buffer, sizeof buffer);
 }
 
 WHIM_UTIL void x11MapWindow(WhimWin* win, whim_bool should_map)
 {
     WhimPayload buffer[2] = {{should_map ? 8 : 10}}; buffer->as_16[1] = WHIM_ARRLEN(buffer); buffer[1].as_32 = win->window_id;
-    WHIM_SEND_REQUEST(win->hook[0], &buffer, sizeof(buffer));
+    whimXcbSendRequest(win->hook, buffer, sizeof buffer);
 }
 
 WHIM_UTIL void x11ReadjustWindow(WhimWin* win)
@@ -423,7 +449,7 @@ WHIM_UTIL void x11ReadjustWindow(WhimWin* win)
 
     buffer[2].as_32 = 1 | 2 | 4 | 8; buffer[3].as_32 = win->rect.x1; buffer[4].as_32 = win->rect.y1; buffer[5].as_32 = win->rect.x2; buffer[6].as_32 = win->rect.y2;
 
-    WHIM_SEND_REQUEST(win->hook[0], &buffer, sizeof(buffer));
+    whimXcbSendRequest(win->hook, buffer, sizeof buffer);
 }
 
 WHIM_UTIL void x11ChangeWindowAttr(WhimWin* win, whim_u32 mask, whim_u32 count, whim_u32 values[])
@@ -433,8 +459,10 @@ WHIM_UTIL void x11ChangeWindowAttr(WhimWin* win, whim_u32 mask, whim_u32 count, 
     buffer[1].as_32 = win->window_id;
     buffer[2].as_32 = mask;
 
-    struct whim_xcb__iovec vecs[4] = {{&buffer, sizeof(buffer)}, {0}, {values, count * sizeof *values}, {0}};
-    x11.sendRequest(win->hook[0].connection, 2, vecs, (struct whim_xcb__req_t[1]){4, 0, 0, 1});
+
+    void *payloads[] = {buffer, values};
+    whim_u32 lengths[] = {sizeof buffer, count * sizeof *values};
+    whimXcbSendMultiRequest(win->hook, WHIM_ARRLEN(payloads), payloads, lengths);
 }
 
 WHIM_UTIL void x11ChangeProperty(WhimWin* win, whim_u32 property, whim_u32 type, whim_u8 format, whim_u32 data_length, const void* data)
@@ -449,8 +477,9 @@ WHIM_UTIL void x11ChangeProperty(WhimWin* win, whim_u32 property, whim_u32 type,
     buffer[4].as_8[0] = format;
     buffer[5].as_32 = data_length;
 
-    struct whim_xcb__iovec vecs[4] = {{&buffer, sizeof(buffer)}, {0}, {(void*)data, data_bytes}, {&buffer, -data_bytes & 3}};
-    x11.sendRequest(win->hook[0].connection, 2, vecs, (struct whim_xcb__req_t[1]){4, 0, 0, 1});
+    void *payloads[] = {buffer, (void*)data};
+    whim_u32 lengths[] = {sizeof buffer, data_bytes};
+    whimXcbSendMultiRequest(win->hook, WHIM_ARRLEN(payloads), payloads, lengths);
 }
 
 WHIM_API(WhimWin* whimWinCreateHooked, X11)(WhimHook *hook, WhimRect rect, const char *title, WhimColor clear_color, enum WhimWinFlags flags)
@@ -459,7 +488,7 @@ WHIM_API(WhimWin* whimWinCreateHooked, X11)(WhimHook *hook, WhimRect rect, const
     if(!win) return 0;
 
     win->hook = hook;
-    win->window_id = x11.generateID(win->hook[0].connection);
+    win->window_id = x11.generateID(win->hook->connection);
     win->rect = rect;
 
     (void)flags;
@@ -472,7 +501,7 @@ WHIM_API(WhimWin* whimWinCreateHooked, X11)(WhimHook *hook, WhimRect rect, const
     x11ReadjustWindow(win);
     x11ChangeProperty(win, x11.atoms.name, x11.atoms.utf8_str, 8, WHIM_STRLEN(title), title);
     x11ChangeProperty(win, x11.atoms.wm_protocols, 4, 32, 1, &x11.atoms.close);
-    x11.flush(win->hook[0].connection);
+    x11.flush(win->hook->connection);
 
     return win;
 }
@@ -484,14 +513,14 @@ WHIM_API(WhimWin* whimWinCreate, X11)(WhimRect rect, const char *title, WhimColo
 
 WHIM_API(void whimWinSetTitle, X11)(WhimWin *win, const char *title) {
     x11ChangeProperty(win, x11.atoms.name, x11.atoms.utf8_str, 8, WHIM_STRLEN(title), title);
-    x11.flush(win->hook[0].connection);
+    x11.flush(win->hook->connection);
 }
 
 WHIM_API(void whimWinDestroy, X11)(WhimWin *win)
 {
     WhimPayload buffer[2] = {{4}}; buffer->as_16[1] = 2; buffer[1].as_32 = win->window_id;
-    WHIM_SEND_REQUEST(win->hook[0], &buffer, sizeof(buffer));
-    x11.flush(win->hook[0].connection);
+    whimXcbSendRequest(win->hook, &buffer, sizeof(buffer));
+    x11.flush(win->hook->connection);
     WHIM_FREE(win);
 }
 
@@ -504,29 +533,30 @@ WHIM_API(void whimDeinit, X11)(void)
 WHIM_UTIL void x11ParseEvent(WhimPayload receiver[], WhimEvent *event)
 {
     enum {KEY_PRESS = 2, KEY_RELEASE = 3, CLIENT_MESSAGE = 33};
-    switch(receiver[0].as_8[0] & (whim_u8)~0x80) {
-        case KEY_PRESS:
-            event->as_key.type = WHIM_EVENT_KEY;
-            event->as_key.keycode = receiver[0].as_8[1] - 8;
-            event->as_key.is_pressed = 1;
+    switch(receiver->as_8[0] & (whim_u8)~0x80) {
+    case KEY_PRESS:
+        event->type = WHIM_EVENT_KEY;
+        event->as_key.keycode = receiver->as_8[1] - 8;
+        event->as_key.is_pressed = 1;
+        break;
+    case KEY_RELEASE:
+        event->type = WHIM_EVENT_KEY;
+        event->as_key.keycode = receiver->as_8[1] - 8;
+        event->as_key.is_pressed = 0;
+        break;
+    case CLIENT_MESSAGE:
+        if(receiver[3].as_32 != x11.atoms.close)
             break;
-        case KEY_RELEASE:
-            event->as_key.type = WHIM_EVENT_KEY;
-            event->as_key.keycode = receiver[0].as_8[1] - 8;
-            event->as_key.is_pressed = 0;
-            break;
-        case CLIENT_MESSAGE:
-            if(receiver[3].as_32 != x11.atoms.close)
-                break;
 
-            event->as_close.type = WHIM_EVENT_CLOSE;
-            event->as_close.window_id = receiver[1].as_32;
-            break;
+        event->type = WHIM_EVENT_CLOSE;
+        event->as_close.window_id = receiver[1].as_32;
+        break;
+    default:
+        event->type = WHIM_EVENT_NONE;
     }
 }
 
 WHIM_API(void whimPollEvents, X11)(WhimEvent *event) { whimHookPollEvents(&x11.hook, event); }
-
 WHIM_API(void whimHookPollEvents, X11)(WhimHook *hook, WhimEvent *event)
 {
     WHIM_ASSERT(event, "Null pointer passed as event");
